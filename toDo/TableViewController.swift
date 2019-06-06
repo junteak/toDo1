de78847ded7fa93353b92d9398d494e1f1813c98
junteak <edamame.zombie@gmail.com>
2019-06-06 14:39:37 +0900

    完成！

--- toDo/TableViewController.swift
+++ toDo/TableViewController.swift
@@ -1,30 +1,42 @@
 
 import UIKit
 
+
 class TableViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
+// 追加画面でへ追加した単語をすぐ反映するリロードコマンド
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // "変数名.count"で要素数を取得できます。
         return word.count
+        
     }
+    
+
 //セクションの数を決める
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-// 各行のセルを表示する際に呼ばれる処理です。
-// この中で表示内容を設定するプログラムを記述します。
+// 各行のセルを表示する処理
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-// セルのオブジェクトを作成します。
-//"NameCell"の部分はStoryboardでセルに設定したIdentifierを指定。
+        
+// セルのオブジェクトを作成
+//"NameCell"の部分はStoryboardでセルに設定したIdentifierを指定
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
 
-// セルのラベルにwordの要素を表示するように指定しています。
-//cell.textLabel はStyleをBasicに指定したセルのラベルです。
+// セルのラベルにwordの要素を表示するように指定
+//cell.textLabel はStyleをBasicに指定したセルのラベル
         cell.textLabel?.text = word[indexPath.row]
         return cell
     }
